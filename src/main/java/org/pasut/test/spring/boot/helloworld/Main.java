2019ad8e740ee159209645e0b16551cffdbead3d
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-02-06 11:04:01 -0300

    agrego un endpoint en Main

--- src/main/java/org/pasut/test/spring/boot/helloworld/Main.java
+++ src/main/java/org/pasut/test/spring/boot/helloworld/Main.java
@@ -1,10 +1,21 @@
 package org.pasut.test.spring.boot.helloworld;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
 public class Main {
 
+	@RequestMapping("/")
+	String hello() {
+		return "Hello World";
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(Main.class, args);
 	}
 
 }
